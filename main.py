89a706e9087901919304101e8980661521d0accd
zthurman <zechariah.thurman@gmail.com>
2025-09-30 21:21:36 -0400

    Start with ingredient parsing

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-import mistletoe
-from mistletoe.latex_renderer import LaTeXRenderer
+from fractions import Fraction
 
+class Ingredient:
+	def __init__(self, ingredient_str: str):
+		self._ingredient = ingredient_str.split()
+		self.quantity = float(Fraction(self._ingredient[0]))
+		self.unit = self._ingredient[1]
+		self.name = " ".join(self._ingredient[2:])
 
-with open("recipe.md", 'r') as recipe:
-    # rendered = mistletoe.markdown(recipe)
-    latex = mistletoe.markdown(recipe, LaTeXRenderer)
+ingredient = "3/4 tsp brown gravy"
+test = Ingredient(ingredient)
+print(test.quantity)
+print(test.unit)
+print(test.name)
 
-# print(rendered)
-print(latex)
