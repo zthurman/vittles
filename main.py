c5e1b403e444fb98c001194b6033c291ac3a7808
zthurman <zechariah.thurman@gmail.com>
2025-10-02 22:10:24 -0400

    Tweaks to the ingredient handler,
    we need to handle separated quantities like
    "1 1/2". Started on next recipe dict. Blew
    out old crusty reqs, made a new one with
    black in it. Hit the file with black format.

--- main.py
+++ main.py
@@ -3,11 +3,41 @@ from fractions import Fraction
 
 
 class Ingredient:
-	def __init__(self, ingredient_str: str):
-		self._ingredient = ingredient_str.split()
-		self.quantity = float(Fraction(self._ingredient[0]))
-		self.unit = self._ingredient[1]
-		self.name = " ".join(self._ingredient[2:])
+    def __init__(self, ingredient_str: str):
+        self._ingredient = ingredient_str.split()
+        self.quantity = " ".join(self.quantity_finder())
+        # We'll assume that after we've nuked all
+        # the quantity info, the thing immediately
+        # after will be the unit.
+        self.unit = self._ingredient[0]
+        # And everything after that is the name
+        self.name = " ".join(self._ingredient[1:])
+
+    def quantity_finder(self):
+        quantity_items = list()
+        # tricky list copy syntax, so we can
+        # poop things off when we determine
+        # they're part of the quantity info
+        for each in self._ingredient[:]:
+            try:
+                # If the first character of the
+                # string is a number we hope it's
+                # part of the ingredient quantity
+                # But if it ends in a % its probably
+	        # a pesky ground beef grade hiding
+	        # at the back of the number so bail
+	        # early if that happens
+                if each[-1] == "%":
+                    continue
+                int(each[0])
+                quantity_items.append(each)
+                self._ingredient.remove(each)
+            except:
+                # They say that this is an anti-pattern,
+                # but in this case it's justified
+                # MUAHAHAHAHAHAHAHA
+                pass
+        return quantity_items
 
 
 ingredient = "3/4 tsp brown gravy"
@@ -16,53 +46,63 @@ print(test.quantity)
 print(test.unit)
 print(test.name)
 
+ingredient = "1 3/4 cup brown gravy"
+test = Ingredient(ingredient)
+print(test.quantity)
+print(test.unit)
+print(test.name)
+
+
 test_recipe = {
-	"Title" : "Crock Pot Picadillo",
-	"Prep Time" : "15 mins",
-	"Cook Time" : "3 hours 12 mins",
-	"Servings" : 11,
-	"Ingredients" : [
-		"2 1/2 lbs 93% lean ground beef",
-		"1 cup minced onion",
-		"1 cup diced red bell peppers",
-		"3 cloves garlic",
-		"1/4 cup minced cilantro",
-		"1 small tomato, diced",
-		"8 oz can tomato sauce",
-		"1/4 cup olives",
-		"1 1/2 tsp ground cumin",
-		"1/4 tsp garlic powder",
-		"2 bay leaves",
-		"1 1/4 cups water",
-	],
-	"Directions" : [
-		"Brown meat in a large skillet on medium-high heat. Use a wooden spoon to break the meat up into small pieces.",
-		"When meat is no longer pink, drain all the liquid from pan. Add the onions, garlic and bell peppers to the meat and cook an addiotional 3-4 minutes.",
-		"Transfer the meat to the slow cooker, then add tomato, cilantro, tomato sauce, water, olives (with some of the brine), then add the spices",
-		"Set slow cooker to HIGH for 3-4 hours or LOW for 6-8 hours."
-	]
+    "Title": "Barbacoa Brisket",
+    "Prep Time": "",
+    "Cook Time": "",
+    "Servings": 8,
+    "Ingredients": [
+        "1 tsp oregano",
+        "1 Tbs dark brown sugar",
+        "2 Tbs olive oil",
+        "1 Tbs minced chipotle chilies in adobo",
+        "1 Tbs adobo sauce",
+        "1 tsp ground cumin",
+        "3 garlic cloves",
+        "1 lbs-ish tritip",
+        "2 medium tomatoes, chopped",
+        "1 medium onion",
+        "1 red bell pepper",
+        "1 jalapeno pepper",
+    ],
+    "Directions": [
+        "",
+    ],
 }
 
 # Specify the filename for your JSON output
-#filename = "examples/crockpot-picadillo.json"
+# filename = "examples/crockpot-picadillo.json"
 
 # Open the file in write mode ('w') and use json.dump()
-#with open(filename, 'w') as json_file:
-#    json.dump(test_recipe, json_file, indent=4) 
+# with open(filename, 'w') as json_file:
+#    json.dump(test_recipe, json_file, indent=4)
+
 
 class Recipe:
-	def __init__(self, input_recipe: dict):
-		self.input_recipe = input_recipe
-		self.recipe_valid = self.verify_input()
-		assert self.recipe_valid, f"Recipe must have: {self.required_keys()}"
+    def __init__(self, input_recipe: dict):
+        self.input_recipe = input_recipe
+        self.recipe_valid = self.verify_input()
+        assert self.recipe_valid, f"self.recipe_keys_message()"
 
-	def required_keys(self):
-		return ["Title", "Ingredients", "Directions"]
+    def recipe_keys_message(self):
+        msg = f"A recipe must have: {self.required_keys()}"
+        return msg
 
-	def verify_input(self):
-		if all(key in self.input_recipe for key in self.required_keys()):
-			return True
-		else:
-			return False
+    def required_keys(self):
+        return ["Title", "Ingredients", "Directions"]
+
+    def verify_input(self):
+        if all(key in self.input_recipe for key in self.required_keys()):
+            return True
+        else:
+            return False
+
 
 test = Recipe(test_recipe)
