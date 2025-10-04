b5395fedfa7a0488faea190baeaadb48ce020cc9
zthurman <zechariah.thurman@gmail.com>
2025-10-03 21:27:54 -0400

    Added hypothesis to the dev-requirements.txt
    and tried and failed to use it.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,19 +1,20 @@
 import unittest
+from hypothesis import given, settings, strategies as st
+from fractions import Fraction
 
 from vittles.ingredient import Ingredient
 
 
 class TestIngredient(unittest.TestCase):
-    def testIngredientBrownGravy(self):
-        ingredient = "3/4 tsp brown gravy"
+    @given(
+        st.integers(),
+        st.floats(allow_nan=False, allow_infinity=False, allow_subnormal=False),
+        st.text(min_size=3, max_size=3),
+        st.text(),
+    )
+    def testIngredientConstructor(self, i, f, unit, name):
+        ingredient = f"{i} {str(Fraction(f))} {unit} {name}"
         test = Ingredient(ingredient)
-        self.assertEqual(test.quantity, "3/4")
-        self.assertEqual(test.unit, "tsp")
-        self.assertEqual(test.name, "brown gravy")
-
-    def testIngredientPintoBeans(self):
-        ingredient = "1 1/3 cup pinto beans"
-        test = Ingredient(ingredient)
-        self.assertEqual(test.quantity, "1 1/3")
-        self.assertEqual(test.unit, "cup")
-        self.assertEqual(test.name, "pinto beans")
+        self.assertEqual(test.quantity, f"{i} {str(Fraction(f))}")
+        self.assertEqual(test.unit, f"{unit}")
+        self.assertEqual(test.name, f"{name}")
