b66b729cabf320c2aa3cf938332b0944b2bd729b
zthurman <zechariah.thurman@gmail.com>
2025-10-03 23:20:39 -0400

    Praise be that Latin-1 exists. I was about
    to actually start yelling at the monitor.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,5 @@
 import unittest
+import re
 from hypothesis import given, settings, strategies as st
 from fractions import Fraction
 
@@ -7,10 +8,26 @@ from vittles.ingredient import Ingredient
 
 class TestIngredient(unittest.TestCase):
     @given(
-        st.integers(),
-        st.floats(allow_nan=False, allow_infinity=False, allow_subnormal=False),
-        st.text(min_size=3, max_size=3),
-        st.text(),
+        st.integers(min_value=1),
+        st.floats(
+            min_value=0.1,
+            max_value=0.99,
+            allow_nan=False,
+            allow_infinity=False,
+            allow_subnormal=False,
+        ),
+        st.text(
+            alphabet=st.characters(
+                codec="latin-1", min_codepoint=0x41, max_codepoint=0x5A
+            ),
+            min_size=1,
+        ),
+        st.text(
+            alphabet=st.characters(
+                codec="latin-1", min_codepoint=0x41, max_codepoint=0x5A
+            ),
+            min_size=1,
+        ),
     )
     def testIngredientConstructor(self, i, f, unit, name):
         ingredient = f"{i} {str(Fraction(f))} {unit} {name}"
